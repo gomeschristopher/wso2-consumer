6a2996ba37ca0f2b4c0b118993d8ef943515c755
Christopher <gomeschristopher@hotmail.com>
2025-06-17 14:12:48 -0300

    configure oauth2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oauth2-angular';
+
+  constructor(private oAuthService: OAuthService,
+    private http: HttpClient) {}
+
+  login() {
+    this.oAuthService.initLoginFlow();
+  }
+
+  logout() {
+    this.oAuthService.revokeTokenAndLogout();
+  }
+
+  get username() {
+    var claims: any = this.oAuthService.getIdentityClaims();
+    if (!claims) return null;
+    return claims.username;
+  }
+
+  fetchUserData() {
+    this.http.get('https://localhost:9443/scim2/Me')
+      .subscribe(response => {
+        console.log(response)
+      }, err => {
+        console.log(err)
+      });
+  }
 }
