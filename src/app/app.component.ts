f9309bcebbdc89bb25d6f532c2bf0a13049d00d6
Christopher <gomeschristopher@hotmail.com>
2025-06-17 15:26:25 -0300

    update to Angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
-import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: true,
-    imports: [NgIf]
+    standalone: true
 })
 export class AppComponent {
   title = 'wso2-consumer';
